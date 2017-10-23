ca869c82a096fbb712cef578874afc3b6704f9c7
roman <roman.shumada@gmail.com>
2017-10-23 22:23:10 +0300

    po 28 with erroors

--- storeapp/add_to_cart.rb
+++ storeapp/add_to_cart.rb
@@ -1,9 +1,11 @@
 require_relative "init"
 
-cart = Cart.new
+cart = Cart.new(ARGV.delete_at(0))
 
 ARGV.each do |a|
   @items.each {|i| cart.add_item(i) if a == i.name}
 end
+#cart.read_from_file
+cart.save_to_file
 
-p cart.items
+#p cart.items
--- storeapp/cart.rb
+++ storeapp/cart.rb
@@ -1,11 +1,27 @@
 class Cart
-  
+
    attr_reader :items
-    
+
    include ItemConteiner
-     
-   def initialize
+
+   def initialize(owner)
       @items = Array.new
+      @owner = owner
    end
-  
+
+   def save_to_file
+     File.open("#{@owner}_cart.txt","w") do |f|
+        @items.each {|i| f.puts "#{i.name}:#{i.price}:#{i.weight}"}#cup:101:250
+     end
+   end
+
+   def read_from_file
+     return unless File.exist?("#{@owner}_cart.txt")
+     item_fields = File.readlines("#{@owner}_cart.txt")#"cup:101:250\n"
+     item_fields.map! {|i| i.chomp }
+     item_fields.map! {|i| i.split(":") }
+     item_fields.each {|i| @items<<RealItem.new(name:i[0],price:i[1].to_f, weight:i[2].to_f)}
+     @items.uniq!
+   end
+
 end
