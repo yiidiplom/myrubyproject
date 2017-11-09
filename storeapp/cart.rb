ec70de9cabd2a99373e0319fa6787094d3c67086
yiidiplom <roman.shumada@zhu.edu.ua>
2017-11-09 22:03:28 +0200

    po 30 all

--- storeapp/cart.rb
+++ storeapp/cart.rb
@@ -16,9 +16,11 @@ class Cart
    end#def
 
    def read_from_file
-     return unless File.exist?("#{@owner}_cart.txt")
-     File.readlines("#{@owner}_cart.txt").each {|i| @items << i.to_real_item}
-     @items.uniq!
-   end
+      File.readlines("#{@owner}_cart.txt").each {|i| @items << i.to_real_item}
+      @items.uniq!
+   rescue Errno::ENOENT
+      File.open("#{@owner}_cart.txt","w")
+      puts "File  #{@owner}_cart.txt was created "
+  end
 
 end
